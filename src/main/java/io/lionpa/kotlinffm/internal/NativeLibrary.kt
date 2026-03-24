852af2a865e209b4564cca8d25e5c729543cc83e
Lion <LionPa2009@gmail.com>
2026-03-24 18:18:58 +0700

    Update NativeLibrary.kt

--- src/main/java/io/lionpa/kotlinffm/internal/NativeLibrary.kt
+++ src/main/java/io/lionpa/kotlinffm/internal/NativeLibrary.kt
@@ -8,18 +8,13 @@ import java.lang.foreign.MemorySegment
 internal object NativeLibrary : Library(linker.defaultLookup()) {
 
     private val calloc = method("calloc", address, long, long)
-    //private val aligned_alloc = method("aligned_alloc", address, long, long)
 
     private val free = void("free", address)
 
     fun calloc(bytes: Long) : MemorySegment {
         return (calloc.invokeExact(1L, bytes) as MemorySegment).reinterpret(bytes)
     }
-
-    //fun alloc(alignment : Long, bytes: Long) : MemorySegment {
-    //    return aligned_alloc.invokeExact(alignment, bytes) as MemorySegment
-    //}
-
+    
     fun free(memorySegment: MemorySegment){
         free.invokeExact(memorySegment)
     }
